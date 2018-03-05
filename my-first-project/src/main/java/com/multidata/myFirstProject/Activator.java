ec70fbe6a9865f6303a9c337859fe714c613695d
MuranteMultidata <alessio.murante@multidata.org>
2018-03-05 16:27:59 +0100

    .

--- my-first-project/src/main/java/com/multidata/myFirstProject/Activator.java
+++ my-first-project/src/main/java/com/multidata/myFirstProject/Activator.java
@@ -5,11 +5,13 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-    public void start(BundleContext context) {
+    public void start(BundleContext context)
+    {
     	System.out.println("My first project START..");
     }
 
-    public void stop(BundleContext context) {
+    public void stop(BundleContext context)
+    {
     	System.out.println("My first project STOP");
     }
 
